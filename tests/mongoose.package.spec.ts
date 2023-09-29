14a9b82b6ea68e7141830b68fe893bc5d7caf85f
John Plusjé <john.plusje@ivengi.com>
2023-09-29 11:06:43 +0200

    Initial commit

---
+++ tests/mongoose.package.spec.ts
@@ -0,0 +1,52 @@
+
+import {describe, test} from '@jest/globals';
+import { model, Schema } from "mongoose";
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('mongoose package', () => {
+  test(`discriminators`, async () => {
+    const options = { discriminatorKey: 'type' };
+
+    interface IEvent  {
+      type: string;
+    }
+
+    const eventSchema = new Schema({date: Schema.Types.Date}, options);
+    const Event = model<IEvent>('Event', eventSchema, );
+
+    interface IClickedLinkEvent extends IEvent {
+      url: string;
+    }
+
+    const clickedLinkEventSchema = new Schema({ url: String }, options);
+    const ClickedLinkEvent = Event.discriminator<IClickedLinkEvent>('ClickedLinkEvent', clickedLinkEventSchema, 'clickedLinkEvent');
+
+    interface IClickedImageEvent extends IEvent {
+      image: string;
+    }
+
+    const clickedImageEventSchema = new Schema({ image: String}, options);
+    const ClickedImageEvent = Event.discriminator<IClickedImageEvent>('ClickedImageEvent', clickedImageEventSchema, 'clickedImageEvent');
+
+
+    const clickedLinkEvent = new ClickedLinkEvent({ url: 'https://clicked-link.com' });
+    expect(clickedLinkEvent.type).to.equal('clickedLinkEvent');
+    expect(clickedLinkEvent.url).to.equal('https://clicked-link.com');
+
+    const clickedImageEvent = new ClickedImageEvent({ image: 'clicked-image.png'});
+    expect(clickedImageEvent.type).to.equal('clickedImageEvent');
+    expect(clickedImageEvent.image).to.equal('clicked-image.png');
+
+    const query = {
+      type: 'clickedLinkEvent',
+      $or: [
+        { type: 'clickedImageEvent' }
+      ]
+    }
+
+    const result = await Event.find(query).exec();
+    expect(result.length).to.equal(0);
+  }, 10000);
+})
